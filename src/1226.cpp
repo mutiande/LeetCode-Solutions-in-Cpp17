8bb69fc546486c5e73839431204927626601dd18
downdemo <downdemo@qq.com>
2020-03-22 19:39:33 +0800

    init

---
+++ src/1226.cpp
@@ -0,0 +1,23 @@
+class DiningPhilosophers {
+public:
+    DiningPhilosophers() {
+        
+    }
+
+    void wantsToEat(int philosopher,
+                    function<void()> pickLeftFork,
+                    function<void()> pickRightFork,
+                    function<void()> eat,
+                    function<void()> putLeftFork,
+                    function<void()> putRightFork) {
+        scoped_lock lk(m[philosopher], m[(philosopher + 1) % 5]);
+        pickLeftFork();
+        pickRightFork();
+        eat();
+        putLeftFork();
+        putRightFork();
+    }
+
+private:
+    mutex m[5];
+};
